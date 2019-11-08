5958c5e8e35972f24f33bd2d79b9668ac9c5e0c6
patrick hoehn <patrh_99@hotmail.com>
2019-11-08 00:27:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {railroads: {rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}, names: {reading_railroad: {}, pennsylvania_railroad: {}, railroad_3: "", railroad_4: ""}}
+  {railroads: {rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}, names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, railroad_4: ""}}
  
     
   }
