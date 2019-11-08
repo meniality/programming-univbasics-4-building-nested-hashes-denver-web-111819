e8b022a7cb060ae0d7fa8553ef66c7701e29b591
patrick hoehn <patrh_99@hotmail.com>
2019-11-08 00:05:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,9 +8,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {railroads: {rent_in_dollars: {one: "1" ,two: "2" ,three: "3" ,four: "4" }, {names: "name"}
+  {railroads: {rent_in_dollars: {one: "1" ,two: "2" ,three: "3" ,four: "4" }}, {names: "name"}
     
-  }
   }
 end
 
